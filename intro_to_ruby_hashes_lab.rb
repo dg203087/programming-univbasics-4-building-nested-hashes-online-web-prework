32157f725d265c51238225982299510d89f440c5
Deanna Kelley <deanna.georges@gmail.com>
2019-09-24 14:40:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,7 @@ end
 
 def monopoly_with_second_tier
 	base_hash = {:railroads => {
-	  :pieces => {
-	    :piece1 => "top_hat", 
-	    :piece2 => "dog", 
-	    :piece3 => "iron", 
-	    :piece4 => "money_bag"
-	  }
+	  :pieces => 4
 	}
 	}
 	 base_hash
@@ -22,16 +17,11 @@ end
 def monopoly_with_third_tier
  	base_hash = {
  	  :railroads => {
-	     :pieces => {
-	        :piece1 => "top_hat", 
-	        :piece2 => "dog", 
-	        :piece3 => "iron", 
-	        :piece4 => "money_bag"
-	      }, 
+	     :pieces => 4,  
 	     :rent_in_dollars => {
-	        :one_piece_owned => 25
-	        :two_piece_owned => 50
-	        :three_piece_owned => 100
+	        :one_piece_owned => 25,
+	        :two_piece_owned => 50,
+	        :three_piece_owned => 100,
 	        :four_piece_owned => 200
 	      }
 	  }
@@ -39,28 +29,3 @@ def monopoly_with_third_tier
 	 base_hash
 end
 
-def monopoly_with_fourth_tier
-	base_hash = {
- 	  :railroads => {
-	     :pieces => {
-	        :piece1 => "top_hat", 
-	        :piece2 => "dog", 
-	        :piece3 => "iron", 
-	        :piece4 => "money_bag"
-	      }, 
-	     :rent_in_dollars => {
-	        :one_piece_owned => 25
-	        :two_piece_owned => 50
-	        :three_piece_owned => 100
-	        :four_piece_owned => 200
-	      }, 
-	     :names => {
-	        :reading_railroad => {morgage value: "$100"}
-	        :pennsylvania_railroad => {morgage value: "$200"}
-	        :b_and_o_railroad => {morgage value: "$400"}
-	        :shortline_railroad => {morgage value: "$800"}
-	     }
-	  }
-	}
-	 base_hash
-end
