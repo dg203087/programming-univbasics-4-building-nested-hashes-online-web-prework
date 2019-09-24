01cb25c2a8b6d66f56b8b915d5c3ede03a3c85ad
Deanna Kelley <deanna.georges@gmail.com>
2019-09-24 14:39:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,66 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+	base_hash = {:railroads => {
+	  
+	}
+	}
+	base_hash
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	base_hash = {:railroads => {
+	  :pieces => {
+	    :piece1 => "top_hat", 
+	    :piece2 => "dog", 
+	    :piece3 => "iron", 
+	    :piece4 => "money_bag"
+	  }
+	}
+	}
+	 base_hash
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+ 	base_hash = {
+ 	  :railroads => {
+	     :pieces => {
+	        :piece1 => "top_hat", 
+	        :piece2 => "dog", 
+	        :piece3 => "iron", 
+	        :piece4 => "money_bag"
+	      }, 
+	     :rent_in_dollars => {
+	        :one_piece_owned => 25
+	        :two_piece_owned => 50
+	        :three_piece_owned => 100
+	        :four_piece_owned => 200
+	      }
+	  }
+	}
+	 base_hash
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+	base_hash = {
+ 	  :railroads => {
+	     :pieces => {
+	        :piece1 => "top_hat", 
+	        :piece2 => "dog", 
+	        :piece3 => "iron", 
+	        :piece4 => "money_bag"
+	      }, 
+	     :rent_in_dollars => {
+	        :one_piece_owned => 25
+	        :two_piece_owned => 50
+	        :three_piece_owned => 100
+	        :four_piece_owned => 200
+	      }, 
+	     :names => {
+	        :reading_railroad => {morgage value: "$100"}
+	        :pennsylvania_railroad => {morgage value: "$200"}
+	        :b_and_o_railroad => {morgage value: "$400"}
+	        :shortline_railroad => {morgage value: "$800"}
+	     }
+	  }
+	}
+	 base_hash
 end
